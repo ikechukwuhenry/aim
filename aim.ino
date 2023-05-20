b4427eb4e5e581d9628200959e41590df96c14d1
ikechukwuhenry <ikechukwuhenry@ymail.com>
2023-05-20 14:04:01 +0100

    add gps location to display

--- aim.ino
+++ aim.ino
@@ -1,24 +1,26 @@
+#include <TinyGPS++.h>                                  // Tiny GPS Plus Library
+#include <SoftwareSerial.h>                             // Software Serial Library so we can use other Pins for communication with the GPS module
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
+                      // Adafruit oled library for display
 #include "DHT.h"
 #define DHTPIN 2    
 #define DHTTYPE DHT11   // DHT 11
-
+                  // OLED display (SDA to Pin 4), (SCL to Pin 5)
 DHT dht(DHTPIN, DHTTYPE);
+static const int RXPin = 13, TXPin = 12;                // Ublox 6m GPS module to pins 12 and 13
+static const uint32_t GPSBaud = 9600;                   // Ublox GPS default Baud Rate is 9600
 
-/* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
-#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+ #define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
 //#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1   //   QT-PY / XIAO
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-
 
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
@@ -55,25 +57,29 @@ String secureUrl;
 String secureUrl2;
 String secureUrl3;
 String secureUrl4;
+float latitude;
+float longitude;
 
 String user = "";
-int S0;
+   #define S0 16
   #define  D0 
- #define SIG A0
+ #define SIG A0                  // Your Home Longitude
+
+TinyGPSPlus gps;                                        // Create an Instance of the TinyGPS++ object called gps
+SoftwareSerial ss(RXPin, TXPin);                        // The serial connection to the GPS device
+
+#include <stdint.h>
 
 
 
 
-  
- 
- void setup(void) { 
-    // Instructions to start the MQ2 sensor
-   //pinMode(pinAout,INPUT); 
-   Serial.begin (9600);
-   Serial.println ();                                    // cut the gibberish first line Serial Monitor
-   Serial.println ();    
-   Serial.print ("Hello sonvisage");                         // test Serial Monitor
-   pinMode(S0,OUTPUT); 
+void setup()
+{  
+                                      // Clear OLED display
+     Serial.begin(9600);                              // Update display
+  delay(1500);                                          // Pause 1.5 seconds  
+  ss.begin(GPSBaud);  
+  pinMode(S0,OUTPUT); 
 
 
   delay(250); // wait for the OLED to power up
@@ -92,13 +98,45 @@ int S0;
   display.println("Failure is always an option");
 
   Serial.println(F("DHTxx test!"));
+  
 
-  dht.begin();
+  dht.begin();// Set Software Serial Comm Speed to 9600    
 }
 
- void loop (){
-   
-    digitalWrite(S0,LOW); 
+void loop()
+{   
+ 
+  Serial.print("Latitude  : ");
+  latitude = gps.location.lat();
+  longitude = gps.location.lng();
+  Serial.println(latitude, 5);
+  Serial.print("Longitude : ");
+  Serial.println(longitude, 4);
+  Serial.print("Satellites: ");
+  Serial.println(gps.satellites.value());
+  Serial.print("Elevation : ");
+ Serial.print(gps.altitude.feet());
+  Serial.println("ft"); 
+  Serial.print("Time UTC  : ");
+  Serial.print(gps.time.hour());                       // GPS time UTC 
+  Serial.print(":");
+  Serial.print(gps.time.minute());                     // Minutes
+  Serial.print(":");
+ Serial.println(gps.time.second());                   // Seconds
+  Serial.print("Heading   : ");
+  Serial.println(gps.course.deg());
+  Serial.print("Speed     : ");
+  Serial.println(gps.speed.mph());
+
+    // Update display
+  delay(200); 
+
+  smartDelay(500);                                      // Run Procedure smartDelay
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+   Serial.println(F("No GPS data received: check wiring"));
+
+digitalWrite(S0,LOW); 
     MQ2 mq2(SIG);
      mq2.begin();
     float* values= mq2.read(true); //if set to "false" will not appear on serial monitor
@@ -136,7 +174,7 @@ int S0;
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-    Serial.print(F("Humidity: "));
+  Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
@@ -148,16 +186,14 @@ int S0;
   Serial.print(hif);
   Serial.println(F("°F"));
 
-
-  // display results on oled screen
-    displayResults(t,h);
+   displayResults(t,h);
     display.display();
     delay(2000);
-    display.clearDisplay();
+   display.clearDisplay();
      delay(1000); 
       delay(1000);  
- } 
 
+}
  void displayResults(float t, float h){
   Serial.println("inside display");
   Serial.println(t);
@@ -165,28 +201,34 @@ int S0;
     display.setTextSize(1);
     display.setTextColor(SH110X_WHITE);
     display.setCursor(0, 0);
+    display.write("Air Monitor", 10);
+    display.println();
+    display.setCursor(0, 20);
     display.write("CO: ");
-    display.print(Co, 5);
+    display.print(Co, 8);
     display.println();
     display.write("Smoke: ");
-    display.print(Smoke, 5);
+    display.print(Smoke, 8);
     display.println();
     display.write("LPG: ");
-    display.print(LPG, 5);
+    display.print(LPG, 8);
     display.println();
-    display.write("Temperature:");
-    display.print(t);
+//    display.write("Temperature:");
+//    display.print(t);
+    display.write("Latitude:");
+    display.print(latitude, 5);
 //    display.print(260, OCT);
-    display.print(F("C "));
+//    display.print(F("C "));
     display.println();
-    display.write("Humidity:");
-    display.print(h);
-    display.print(F("%"));
+    display.write("Longitude:");
+    display.print(longitude, 4);
+//    display.write("Humidity:");
+//    display.print(h);
+//    display.print(F("%"));
     display.println();
     display.display();
     delay(1);
 }
-
  void displayTitle(void){
     display.setTextSize(1);
     display.setTextColor(SH110X_WHITE);
@@ -195,7 +237,12 @@ int S0;
     display.display();
     delay(1);
 }
-
- 
-  
-  
+static void smartDelay(unsigned long ms)                // This custom version of delay() ensures that the gps object is being "fed".
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
